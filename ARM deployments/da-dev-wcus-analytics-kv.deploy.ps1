ad969f175d65152affc8ae31a7fccc7c715b2ae2
OptimChain <84352976+OptimChain@users.noreply.github.com>
2021-10-26 09:27:54 -0700

    Add files via upload

---
+++ ARM deployments/da-dev-wcus-analytics-kv.deploy.ps1
@@ -0,0 +1,61 @@
+﻿# -----------------------------------------------------------------------
+# Bartell Drugs  - Azure Deployment Automation
+#   Azure Platform
+#   Enterprise Services dev - WUS2 Development Key vault
+#   Development Environment Deployment
+#
+# 18 may 2020
+# Daniel Nayberger
+# Neudesic, LLC
+# -----------------------------------------------------------------------
+
+#Ensure Subscription is set to Bartell Drugs da-dev Subscription
+$subscriptionId = "57b50573-d245-486d-b3b3-7a5e83dacbb8"
+Set-AzContext -Subscriptionid $subscriptionId
+
+$filePath = "C:\Users\Jason.Bian\Desktop\Solution\Powershell\deployments\Azure Automation\"
+$resourceName = "da-dev-jason-analytics"
+$templateFilePath = $filePath + $resourceName + ".template.json"
+$templateParameterFilePath = $filePath + $resourceName + ".parameters.json"
+
+$resourceGroupName = "da-dev-wus2-analytics-rg"
+$resourceGroupLocation = [string]"westus2"
+
+# Create Network Resource Group
+#Create or check for existing resource group
+$resourceGroup = Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if (!$resourceGroup) {
+    Write-Host "Resource group '$resourceGroupName' does not exist.";
+    Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
+
+    New-AzResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
+    $resourceTags = @{ `
+            "Group Name"      = ""; `
+            "Budget Code"     = ""; `
+            "Project ID"      = ""; `
+            "Deployed By"     = "Daniel.Nayberger@bartelldrugs.onmicrosoft.com"; `
+            "Internal Owner"  = ""; `
+            "Support Contact" = ""; `
+            "SL"              = "1 High Priority"; `
+            "PL"              = "1 Confidential"; 
+    }
+
+    # Assign tags to the resource group
+    Set-AzResourceGroup -Name $resourceGroupName -Tag $resourceTags
+}
+else {
+    Write-Host "Using existing resource group '$resourceGroupName'";
+}
+
+# Start the deployment
+Write-Host "Starting deployment...";
+$timestamp = ((Get-Date).ToString("MM-dd-yyyy-hh-mm-ss"))
+$deploymentName = $resourceName + "-" + $timestamp
+
+if (Test-Path $templateParameterFilePath) {
+    New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name $deploymentName -TemplateFile $templateFilePath -TemplateParameterFile $templateParameterFilePath -Mode Incremental -Verbose -ErrorVariable ErrorMessages;
+}
+else {
+    New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name $deploymentName -TemplateFile $templateFilePath -Mode Incremental -Verbose -ErrorVariable ErrorMessages;
+}
+$ErrorMessages
---
+++ ARM deployments/dadevwus2analyticsdl.deploy.ps1
@@ -0,0 +1,51 @@
+# -----------------------------------------------------------------------
+# Bartell Drugs  - Azure Deployment Automation
+#   Azure Platform
+#   BI and Analytics - Data Lake Gen 2
+#   Development Environment Deployment
+#
+# 22 August 2019
+# Chris Kurt
+# Neudesic, LLC
+# -----------------------------------------------------------------------
+
+function Get-UniqueString ([string]$id, $length = 4) {
+    $hashArray = (new-object System.Security.Cryptography.SHA512Managed).ComputeHash($id.ToCharArray())
+    -join ($hashArray[1..$length] | ForEach-Object { [char]($_ % 26 + [byte][char]'a') })
+}
+#******************************************************************************
+#Ensure Subscription is set to Bartell Drugs da-dev Subscription
+$subscriptionId = "57b50573-d245-486d-b3b3-7a5e83dacbb8"
+Set-AzContext -Subscriptionid $subscriptionId
+
+$filePath = "C:\Users\Jason.Bian\Desktop\Solution\Powershell\deployments\Azure Automation\"
+
+$resourceName = "dadevwus2analyticsdl"
+$uniqueName = $resourceName + (Get-UniqueString -id $(Get-AzResourceGroup $resourceGroupName).ResourceID)
+$templateFilePath = $filePath + $resourceName + ".template.json"
+$templateParameterFilePath = $filePath + $resourceName + ".parameters.json"
+
+$resourceGroupName = "da-dev-wus2-analytics-rg"
+$resourceGroupLocation = "westus2"
+
+#Create or check for existing resource group
+$resourceGroup = Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if (!$resourceGroup) {
+    Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
+    New-AzResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
+}
+else {
+    Write-Host "Using existing resource group '$resourceGroupName'";
+}
+
+# Start the deployment
+Write-Host "Starting deployment...";
+$timestamp = ((Get-Date).ToString("MM-dd-yyyy-hh-mm-ss"))
+$deploymentName = "dadevwus2analyticsdl" + $timestamp
+
+if (Test-Path $templateParameterFilePath) {
+    New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -storageAccountName $uniqueName -TemplateFile $templateFilePath -TemplateParameterFile $templateParameterFilePath -Mode Incremental -Verbose;
+}
+else {
+    New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -storageAccountName $uniqueName -TemplateFile $templateFilePath -Mode Incremental -Verbose;
+}
