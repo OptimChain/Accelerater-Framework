2c318956f500dc2d0cfa3fa07099a167e1077834
OptimChain <84352976+OptimChain@users.noreply.github.com>
2021-11-01 23:04:00 -0700

    Added Framework Values + Working Build
    Added Framework Values + Working Build

--- Azure Databricks Notebooks/ImportDatabricksFolder.ps1
+++ Azure Databricks Notebooks/ImportDatabricksFolder.ps1
@@ -10,10 +10,10 @@ $VaultName = 'da-dev-jason-analytics'
 #$Ctx = New-AzStorageContext -StorageAccountName dadevwus2analyticsdlbrbw -StorageAccountKey "MYopkDS5qb4KVNR2Krf7GZLy/B2VIkKQn0AbiTEiQAr0fllMW+5AdhG6BayIMD6hShBtkRK2E6o7+ZErhUItuQ=="
 
 $FrameworkBearerToken = "dapie694e85ad5fa1294bc2e4d67f3638fef"
-$ContainerBearerToken = "dapie694e85ad5fa1294bc2e4d67f3638fef"
+$ContainerBearerToken = "dapi38e33c52fb5c497e229b5aabdf6780ad"
 
 
-Import-DatabricksFolder -BearerToken $FrameworkBearerToken -Region $Region -LocalPath 'C:\Users\Jason.Bian\Desktop\Solution\Azure Databricks Notebooks\Framework\' -DatabricksPath '/Framework'
+#Import-DatabricksFolder -BearerToken $FrameworkBearerToken -Region $Region -LocalPath #'C:\Users\Jason.Bian\Desktop\Solution\Azure Databricks Notebooks\Framework\' -DatabricksPath '/Framework'
 
 Import-DatabricksFolder -BearerToken $ContainerBearerToken -Region $Region -LocalPath 'C:\Users\Jason.Bian\Desktop\Solution\Azure Databricks Notebooks\Container\' -DatabricksPath '/Container'
 
